28f12d6ba64136298e7b94b533784c181a216723
Melanie Tosik <melanie.tosik@gmail.com>
2017-02-27 11:32:33 -0600

    PEP-8 formatting

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import (
+    Api,
+)
 from flask_restful_swagger import swagger
 
 from app.resources.healthcheck import HealthCheck
 
-# Declare name of app object and create an instance as an API.
+# Declare name of app object and create an instance as an API
 app = Flask(__name__)
-api = swagger.docs(Api(app), apiVersion="0.1")
+api = swagger.docs(Api(app), apiVersion='0.1')
 
-# Setup all your rest resources
+# Set up all your rest resources
 api.add_resource(HealthCheck, '/health-check')
